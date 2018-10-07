eba80d97a9f16307cf0defef418fc1d6e5512993
Jim Tyhurst <jim@tyhurst.com>
2018-10-07 11:52:01 -0700

    Refactored 'wait_for_postgres'. Added test cases.

--- R/wait_for_postgres.R
+++ R/wait_for_postgres.R
@@ -3,28 +3,42 @@ library(DBI)
 
 #' Connect to Postgres, waiting if it is not ready
 #'
-#' @param user Username that will be found
+#' Assumptions: host = "localhost" and port = "5432".
+#' @param user Username for connecting to the database
 #' @param password Password that corresponds to the username
-#' @param dbname the name of the database in the database
-#' @param seconds_to_test the number of iterations to try while waiting for Postgres to be ready
+#' @param dbname the name of the database within the database server
+#' @param seconds_to_test the number of iterations to try while waiting for PostgreSQL service to be ready.
+#' @return When successful: a connection object, which is an S4 object that inherits from DBIConnection used to communicate with the database engine; Otherwise, an error string.
 #' @export
-wait_for_postgres <- function(user, password, dbname, seconds_to_test = 10) {
-  for (i in 1:seconds_to_test) {
-    db_ready <- DBI::dbCanConnect(RPostgres::Postgres(),
-                                  host = "localhost",
-                                  port = "5432",
-                                  user = user,
-                                  password = password,
-                                  dbname = dbname)
-    if ( !db_ready ) {Sys.sleep(1)}
-    else {conn <- DBI::dbConnect(RPostgres::Postgres(),
-                                host = "localhost",
-                                port = "5432",
-                                user = user,
-                                password = password,
-                                dbname = dbname)
+wait_for_postgres <-
+  function(user, password, dbname, seconds_to_test = 10) {
+    conn <- NULL
+    db_ready <- FALSE
+    for (i in 1:abs(seconds_to_test)) {
+      db_ready <- DBI::dbCanConnect(
+        RPostgres::Postgres(),
+        host = "localhost",
+        port = "5432",
+        user = user,
+        password = password,
+        dbname = dbname
+      )
+      if (!db_ready) {
+        Sys.sleep(1)
+      }
+      else {
+        conn <- DBI::dbConnect(
+          RPostgres::Postgres(),
+          host = "localhost",
+          port = "5432",
+          user = user,
+          password = password,
+          dbname = dbname
+        )
+      }
     }
-    if (i == seconds_to_test & !db_ready) {conn <- "There is no connection"}
+    if (!db_ready) {
+      conn <- "There is no connection"
+    }
+    conn
   }
-  conn
-}
--- tests/testthat/test_wait_for_postgres.R
+++ tests/testthat/test_wait_for_postgres.R
@@ -1,12 +1,34 @@
 
-context("Trying to connect to a PostgreSQL instance")
+context("Connecting to a PostgreSQL instance")
 
-test_that("Error message when unable to connect", {
+EXPECTED_ERROR_MESSAGE = "There is no connection"
+
+test_that("Expects error message when unable to connect", {
   conn <- wait_for_postgres(
-    "unknown_user",
-    "some_password",
-    "unknown_database",
-    -2
+    user = "unknown_user",
+    password = "some_password",
+    dbname = "unknown_database",
+    seconds_to_test = 3
   )
-  expect_equal(conn, "There is no connection")
+  expect_equal(conn, EXPECTED_ERROR_MESSAGE)
+})
+
+test_that("Expects negative 'seconds_to_test' is ignored", {
+  conn <- wait_for_postgres(
+    user = "unknown_user",
+    password = "some_password",
+    dbname = "unknown_database",
+    seconds_to_test = -1
+  )
+  expect_equal(conn, EXPECTED_ERROR_MESSAGE)
+})
+
+test_that("Handles 0 seconds_to_test", {
+  conn <- wait_for_postgres(
+    user = "unknown_user",
+    password = "some_password",
+    dbname = "unknown_database",
+    seconds_to_test = 0
+  )
+  expect_equal(conn, EXPECTED_ERROR_MESSAGE)
 })
