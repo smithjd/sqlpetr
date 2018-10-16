827b3c02b847507ac1711369d1180285d9756201
John David Smith <john.smith@learningalliances.net>
2018-10-15 17:00:08 -0700

    Adding a convenience function: sp_preview_chapter

---
+++ R/sp_preview_chapter.R
@@ -0,0 +1,13 @@
+#' Compile and browse an individual chapter
+#'
+#' @param chapter_file name of the local file to preview
+#'
+#' @return browse
+#' @importFrom bookdown preview_chapter
+#' @export
+#'
+#' @examples # sp_preview_chapter("chapter_file.Rmd")
+sp_preview_chapter <- function(chapter_file) {
+  preview_url <- preview_chapter(chapter_file)
+  browseURL(preview_url)
+}
