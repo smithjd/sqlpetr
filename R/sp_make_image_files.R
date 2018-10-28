c1e6fc2f5eabfa756b515fd5255d9c554cc4ae78
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-10-28 00:09:30 -0700

    use Rd markup!!

--- R/sp_make_image_files.R
+++ R/sp_make_image_files.R
@@ -1,39 +1,44 @@
 #' @title Make Image Files
 #' @name sp_make_image_files
-#' @description Creates image files from a DiagrammeR `htmlwidget` object
+#' @description Creates image files from a DiagrammeR \code{htmlwidget} object
 #' @importFrom DiagrammeRsvg export_svg
 #' @importFrom magick image_read_svg
 #' @importFrom magick image_write
 #' @importFrom dplyr %>%
 #' @export sp_make_image_files
-#' @param widget an `htmlwidget` object returned from DiagrammeR, usually from
-#' `grViz` or `render_graph`
-#' @param directory (character) a valid file path. Default is ".". If it does not
-#' exist it will be created.
-#' @param filename (character) a valid file name (stem only). Three files will be
-#' overwritten if they exist and created if they don't:
+#' @param widget an \code{htmlwidget} object returned from DiagrammeR, usually
+#' from \code{grViz} or \code{render_graph}. Note that this does \emph{not}
+#' work for \code{mermaid.js} widgets!
+#' @param directory (character) a valid directory name. It will be created if
+#' it does not exist.
+#' @param filename (character) a valid file name (stem only). Three files will
+#' be overwritten if they exist and created if they don't:
 #' \itemize{
 #' \item <directory>/<filename>.svg
 #' \item <directory>/<filename>.png
 #' \item <directory>/<filename>.pdf
 #' }
 #' @return png_name (character) the name of the PNG file. This is the path you
-#' give to `knitr::include_graphics` for the `auto_pdf` option.
-#' @details See the vignette "Using DiagrammeR with Bookdown" for the derivation
-#' and some examples.
+#' give to \code{knitr::include_graphics} for the \code{auto_pdf} option.
+#' @details See the vignette "Using DiagrammeR with Bookdown" for the
+#' derivation and some examples.
 
-sp_make_image_files <- function(
-  widget, directory = "diagrams", filename = "example") {
+sp_make_image_files <- function(widget, directory, filename) {
 
   dir.create(directory, recursive = TRUE)
   svg_name <- paste(
-    directory, paste(filename, "svg", sep = "."), sep = "/")
+    directory,
+    paste(filename, "svg", sep = "."),
+  sep = "/")
   png_name <- paste(
-    directory, paste(filename, "png", sep = "."), sep = "/")
+    directory,
+    paste(filename, "png", sep = "."),
+  sep = "/")
   pdf_name <- paste(
-    directory, paste(filename, "pdf", sep = "."), sep = "/")
-  widget %>%  DiagrammeRsvg::export_svg() %>%
-    cat(file = svg_name)
+    directory,
+    paste(filename, "pdf", sep = "."),
+  sep = "/")
+  widget %>%  DiagrammeRsvg::export_svg() %>% cat(file = svg_name)
   magick::image_read_svg(svg_name) %>%
     magick::image_write(path = png_name, format = "png")
   magick::image_read_svg(svg_name) %>%
