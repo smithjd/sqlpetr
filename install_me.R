c443d47ff82bfdaf0c3f4449b87f6fc6f3025154
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-10-28 03:27:17 -0700

    add a ,nojekyll

--- install_me.R
+++ install_me.R
@@ -4,3 +4,6 @@ devtools::install(
   ".", dependencies = TRUE, quiet = TRUE, build_vignettes = TRUE)
 pkgdown::clean_site()
 pkgdown::build_site(lazy = FALSE)
+
+# flag this as a non-Jekyll site for GitHub Pages
+file.create("./docs/.nojekyll")
