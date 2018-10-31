763d90d460caaddb92b84a52154c298b012d9937
John David Smith <john.smith@learningalliances.net>
2018-10-31 12:03:56 -0700

    Adding View(df) to sp_print_df.R function

--- R/sp_print_df.R
+++ R/sp_print_df.R
@@ -1,19 +1,24 @@
-#' Print a data frame appropriate to output type
+#' Show (print) a data frame depending on appropriate to output type
 #'
+#' @param df the data frame to be shown
 #'
-#'
-#' @param df the data frame to be printed
-#'
-#' @return either kable (in PDF) or datatable (in HTML) output
+#' @return either kable (in PDF) or datatable (in HTML) output or View when executing chunks in an .Rmd file
 #' @importFrom DT datatable
 #' @importFrom knitr kable
+#' @importFrom rlang enexprs
+#' @importFrom utils View
 #' @export
 #'
 #' @examples # sp_print_df(df)
 sp_print_df <- function(df){
+  df_name <- rlang::enexprs(df)
   if (knitr::is_latex_output()) {
     knitr::kable(df)
-  } else {
+  }
+  else if (knitr::is_html_output()) {
     DT::datatable(df)
   }
+  else {
+    View(df, title = as.character(df_name[[1]]))
   }
+}
