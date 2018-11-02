ff6f35c708ddc39a4d5ce3d590ebc3f2c071d307
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-01 22:35:26 -0700

    remove "View" logic issue #17

--- R/sp_print_df.R
+++ R/sp_print_df.R
@@ -1,24 +1,17 @@
-#' Show (print) a data frame depending on appropriate to output type
+#' Show (print) a data frame depending on appropriate output type
 #'
 #' @param df the data frame to be shown
 #'
-#' @return either kable (in PDF) or datatable (in HTML) output or View when executing chunks in an .Rmd file
+#' @return either kable (in PDF) or datatable (in HTML) output
 #' @importFrom DT datatable
 #' @importFrom knitr kable
-#' @importFrom rlang enexprs
-#' @importFrom utils View
 #' @export
 #'
 #' @examples # sp_print_df(df)
 sp_print_df <- function(df){
-  df_name <- rlang::enexprs(df)
   if (knitr::is_latex_output()) {
     knitr::kable(df)
-  }
-  else if (knitr::is_html_output()) {
+  } else {
     DT::datatable(df)
-  }
-  else {
-    View(df, title = as.character(df_name[[1]]))
   }
 }
