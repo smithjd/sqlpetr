be03b5f51084ec0da447a34ca86bea330086ab4f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-30 15:43:40 -0800

    fix runs outside of RStudio

--- install_me.R
+++ install_me.R
@@ -1,6 +1,16 @@
 #! /usr/bin/env Rscript
 
-install.packages("devtools")
+CRAN <- "https://cloud.r-project.org/" # CRAN URL
+LIB <- Sys.getenv("R_LIBS_USER") # library path
+
+# create library if it doesn't exist
+if (!dir.exists(LIB)) {
+  dir.create(LIB, recursive = TRUE, mode = '0755')
+}
+.libPaths(LIB) # add to search path
+
+update.packages(ask = FALSE, lib = LIB, repos = CRAN, quiet = TRUE)
+install.packages("devtools", lib = LIB, repos = CRAN, quiet = TRUE)
 devtools::install(
   ".", dependencies = TRUE, quiet = TRUE, build_vignettes = TRUE)
 pkgdown::clean_site()
