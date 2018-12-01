b9faf3be5cad2833d18d60445fc87075c2b75f6f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-30 22:25:22 -0800

    make sure PostgreSQL library headers are there

--- debian_stretch_install.bash
+++ debian_stretch_install.bash
@@ -3,6 +3,8 @@
 sudo apt-get install -qqy --no-install-recommends \
   libmagick++-dev \
   libpoppler-cpp-dev \
+  libpq-dev \
+  libpqxx-dev \
   libv8-3.14-dev \
   qpdf
 ./install_me.R
--- ubuntu_bionic_install.bash
+++ ubuntu_bionic_install.bash
@@ -4,6 +4,7 @@ sudo apt-get install -qqy --no-install-recommends \
   libmagick++-dev \
   libpoppler-cpp-dev \
   libpq-dev \
+  libpqxx-dev \
   libv8-3.14-dev \
   qpdf
 sudo sed -i.bak '/<policy domain="coder" rights="none" pattern="PDF" \/>/d' /etc/ImageMagick-6/policy.xml
