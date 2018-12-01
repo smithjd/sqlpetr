d800d052d70a98205b1bf90a44c4e7e520fe6909
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-30 23:19:46 -0800

    make a backup of ImageMagick policy file

--- arch_install.bash
+++ arch_install.bash
@@ -7,5 +7,5 @@ yaourt --aur --sync --noconfirm --needed \
   poppler \
   qpdf \
   v8-3.14
-sudo sed -i 's/,PDF,/,/' /etc/ImageMagick-7/policy.xml
+sudo sed -i.bak 's/,PDF,/,/' /etc/ImageMagick-7/policy.xml
 ./install_me.R
