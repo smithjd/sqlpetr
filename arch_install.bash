eff0a7c65c549f2d3754874eeeac65419502c83a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-30 20:08:36 -0800

    R Markdown needs qpdf

--- arch_install.bash
+++ arch_install.bash
@@ -4,6 +4,7 @@ echo "You will need the Arch User Repository and 'yaourt'!!"
 yaourt --aur --sync --noconfirm --needed \
   libmagick \
   libpqxx \
+  qpdf \
   v8-3.14
 sudo sed -i 's/,PDF,/,/' /etc/ImageMagick-7/policy.xml
 ./install_me.R
