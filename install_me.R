87635ee94ebff71506b34fff92c400cacb8fa0f0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-06 17:44:55 -0800

    cleaning up for Windows and Mac

--- install_me.R
+++ install_me.R
@@ -1,18 +1,6 @@
-#! /usr/bin/env Rscript
-
-CRAN <- "https://cloud.r-project.org/" # CRAN URL
-LIB <- Sys.getenv("R_LIBS_USER") # library path
-
-# create library if it doesn't exist
-if (!dir.exists(LIB)) {
-  dir.create(LIB, recursive = TRUE, mode = '0755')
-}
-.libPaths(LIB) # add to search path
-
-update.packages(ask = FALSE, lib = LIB, repos = CRAN, quiet = TRUE)
-install.packages("devtools", lib = LIB, repos = CRAN, quiet = TRUE)
-devtools::install(
-  ".", dependencies = TRUE, quiet = TRUE, build_vignettes = TRUE)
+update.packages(ask = FALSE)
+install.packages("devtools", quiet = TRUE)
+devtools::install(dependencies = TRUE, build_vignettes = TRUE)
 pkgdown::clean_site()
 pkgdown::build_site(lazy = FALSE)
 
