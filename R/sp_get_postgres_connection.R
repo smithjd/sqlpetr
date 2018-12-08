182947b7d6cf5abd0842824521ba0b1a482d7566
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-08 14:43:26 -0800

    throw error on failure after iterations exhausted

--- R/sp_get_postgres_connection.R
+++ R/sp_get_postgres_connection.R
@@ -5,25 +5,40 @@
 #' @param user Username for connecting to the database
 #' @param password Password that corresponds to the username
 #' @param dbname The name of the database within the database server
-#' @param seconds_to_test The number of iterations to try while waiting for PostgreSQL service to be ready.
-#' @param host The IP address or host where postgreSQL is located, defaults to "localhost"
-#' @param port The port on the host that postgreSQL listens to, defaults to "5432"
+#' @param seconds_to_test The number of iterations to try while waiting
+#' for PostgreSQL service to be ready.
+#' @param host The IP address or host where postgreSQL is located,
+#' defaults to "localhost"
+#' @param port The port on the host that postgreSQL listens to, defaults
+#' to "5432"
 #'
-#' @return When successful: a connection object, which is an S4 object that inherits from DBIConnection used to communicate with the database engine; Otherwise, an error string.
+#' @return If successful: a connection object, which is an S4 object
+#' that inherits from DBIConnection, used to communicate with the
+#' database engine. If unsuccessful, the function terminates with an
+#' error message.
 #'
 #' @examples con <- sp_get_postgres_connection("postgres", "postgres")
 #'
 #' @export
 sp_get_postgres_connection <- function(user, password, dbname,
-           host = "localhost",
-           port = "5432",
-           seconds_to_test = 10) {
-    conn <- NULL
-    db_ready <- FALSE
-    n_iterations <- abs(seconds_to_test)
-    i <- 1
-    while (i <= n_iterations & is.null(conn)) {
-      db_ready <- DBI::dbCanConnect(
+                                       host = "localhost",
+                                       port = "5432",
+                                       seconds_to_test = 10) {
+
+  n_iterations <- abs(seconds_to_test)
+  for (iter in 1:n_iterations) {
+    db_ready <- DBI::dbCanConnect(
+      RPostgres::Postgres(),
+      host = host,
+      port = port,
+      user = user,
+      password = password,
+      dbname = dbname
+    )
+
+    # return a connection if it worked
+    if (db_ready) {
+      conn <- DBI::dbConnect(
         RPostgres::Postgres(),
         host = host,
         port = port,
@@ -31,23 +46,13 @@ sp_get_postgres_connection <- function(user, password, dbname,
         password = password,
         dbname = dbname
       )
-      if (!db_ready) {
-        Sys.sleep(1)
-      }
-      else {
-        conn <- DBI::dbConnect(
-          RPostgres::Postgres(),
-          host = host,
-          port = port,
-          user = user,
-          password = password,
-          dbname = dbname
-        )
-      }
-      i <- i + 1
+      return(conn)
     }
-    if (is.null(conn)) {
-      conn <- "There is no connection"
-    }
-    conn
+
+    # database isn't ready - sleep and retry
+    Sys.sleep(1)
   }
+
+  # couldn't connect - throw an error
+  stop(paste("Database is not ready - reason:", attr(db_ready, "reason")))
+}
