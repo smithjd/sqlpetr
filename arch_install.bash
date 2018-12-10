9c9ae16c3cb54c728aa26f9c01c8f8cb4bbecfc8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-10 05:02:04 -0800

    more deletions

--- arch_install.bash
+++ arch_install.bash
@@ -2,9 +2,7 @@
 
 echo "You will need the Arch User Repository and 'yaourt'!!"
 yaourt --aur --sync --noconfirm --needed \
-  libmagick \
   libpqxx \
-  poppler \
   postgresql-libs \
   qpdf
 sudo sed -i.bak 's/,PDF,/,/' /etc/ImageMagick-7/policy.xml
