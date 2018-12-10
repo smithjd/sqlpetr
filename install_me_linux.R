4670d3a7881a921899a81a5fd6c8d8071a2f84c8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-10 13:29:36 -0800

    more documentation comments

--- install_me_linux.R
+++ install_me_linux.R
@@ -1,5 +1,15 @@
 #! /usr/bin/env Rscript
 
+# Why is this script so much more complicated than `install_me.R`?
+#
+# `install_me.R` is designed to work in RStudio, using the `Source` button
+# on the script development tab. RStudio provides some default environment
+# settings to make that possible,
+#
+# This script is designed to run on a Linux system, called from a `bash` script
+# that installs the distro-specific Linux packages it needs. So it needs to
+# create the personal library if it doesn't exist and update packages first.
+#
 # create personal library if it doesn't exist
 CRAN <- "https://cloud.r-project.org/" # CRAN URL
 LIB <- Sys.getenv("R_LIBS_USER") # library path
@@ -8,7 +18,9 @@ if (!dir.exists(LIB)) {
 }
 .libPaths(LIB) # add to search path
 
-cat("\nUpdating packages into user library\n")
+cat("\nUpdating packages into user library")
+cat("\nYou can ignore warnings about not updating packages")
+cat("\nThis can take some time\n")
 update.packages(ask = FALSE, instlib = LIB, repos = CRAN, quiet = TRUE)
 install.packages("devtools", lib = LIB, repos = CRAN, quiet = TRUE)
 devtools::install(dependencies = TRUE, quiet = TRUE)
