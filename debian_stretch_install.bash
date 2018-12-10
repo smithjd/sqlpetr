9fe79542bdf5a00b0e7ea9bb3bf740b6d65f9e1c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-10 13:29:15 -0800

    magick, DiagrammeRsvg, pdftools and rsvg are no longer required

--- debian_stretch_install.bash
+++ debian_stretch_install.bash
@@ -2,10 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -qqy --no-install-recommends \
-  libmagick++-dev \
-  libpoppler-cpp-dev \
   libpq-dev \
   libpqxx-dev \
-  libv8-3.14-dev \
   qpdf
-R -e "source('install_me_linux.R')"
+./install_me_linux.R
