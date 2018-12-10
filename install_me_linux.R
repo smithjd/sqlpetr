867803fd0ac442f6dc6f5bc6acc32dc2e96356e9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-09 22:59:27 -0800

    clean up package updates

--- install_me_linux.R
+++ install_me_linux.R
@@ -1,13 +1,14 @@
+#! /usr/bin/env Rscript
+
+# create personal library if it doesn't exist
 CRAN <- "https://cloud.r-project.org/" # CRAN URL
 LIB <- Sys.getenv("R_LIBS_USER") # library path
-
-# create library if it doesn't exist
 if (!dir.exists(LIB)) {
   dir.create(LIB, recursive = TRUE, mode = '0755')
 }
 .libPaths(LIB) # add to search path
 
-update.packages(ask = FALSE, lib = LIB, repos = CRAN, quiet = TRUE)
+update.packages(ask = FALSE, instlib = LIB, repos = CRAN, quiet = TRUE)
 install.packages("devtools", lib = LIB, repos = CRAN, quiet = TRUE)
 devtools::install(
   dependencies = TRUE, quiet = TRUE, build_vignettes = TRUE
