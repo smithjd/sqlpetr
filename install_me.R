6ab192336a1eb2e1d9276131a889a49c08579de2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-11 17:50:42 -0800

    test for already installed tinytex

--- install_me.R
+++ install_me.R
@@ -1,7 +1,7 @@
 update.packages(ask = FALSE)
 install.packages("devtools")
 devtools::install(dependencies = TRUE, build_vignettes = TRUE)
-tinytex::install_tinytex()
+if (tinytex::tinytex_root() == "") tinytex::install_tinytex()
 webshot::install_phantomjs()
 pkgdown::clean_site()
 pkgdown::build_site(lazy = FALSE)
