9de561504702ddabe07039a29868766e6c4fab08
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-11 16:48:12 -0800

    install tinytex and phantomjs

--- install_me.R
+++ install_me.R
@@ -1,5 +1,8 @@
+update.packages(ask = FALSE)
 install.packages("devtools")
 devtools::install(dependencies = TRUE, build_vignettes = TRUE)
+tinytex::install_tinytex()
+webshot::install_phantomjs()
 pkgdown::clean_site()
 pkgdown::build_site(lazy = FALSE)
 
