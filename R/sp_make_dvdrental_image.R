f765bfe2f0b21cc41927d6db8ae2e0844dceaf5a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-01-11 00:33:18 -0800

    doc update

--- R/sp_make_dvdrental_image.R
+++ R/sp_make_dvdrental_image.R
@@ -9,6 +9,19 @@
 #'
 #' @examples
 #' \dontrun{sp_make_dvdrental_image("dvdrental")}
+#' @details
+#' This function executes a `docker build` with build context of this
+#' package's `extdata/docker` directory. In RStudio package development,
+#' it's `inst/extdata/docker`.
+#'
+#' The Dockerfile fetches the PostgreSQL 10 image, then installs `unzip`. It then
+#' downloads and unzips the DVD rental database. Then it copyies a restore
+#' script into the container's directory `/docker-entrypoint-initdb.d/restoredb.sh`.
+#'
+#' This script will run when a container first runs the image. It simply creates
+#' the `dvdrental` database and restores the unzipped `tar` archive to it with
+#' `pg_restore`.
+
 sp_make_dvdrental_image <- function(image_name) {
   docker_cmd <- glue::glue(
     "build ", # docker build
