33b6dd9e46a862c1f37af5db2c7552e565373456
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-01-10 20:02:37 -0800

    coding done for image creation

---
+++ R/sp_make_dvdrental_image.R
@@ -0,0 +1,25 @@
+#' Create an image with PostgreSQL and the `dvdrental` database
+#'
+#' @param image_name name for the docker image
+#'
+#' @return 0
+#' @importFrom glue glue
+#' @export
+#'
+#' @examples
+#' \dontrun{sp_make_dvdrental_image("dvdrental")}
+sp_make_dvdrental_image <- function(image_name) {
+  docker_cmd <- glue::glue(
+    "build ", # docker build
+    "--tag ", image_name, # gives the image a name
+    system.file(package = "sqlpetr"), "/extdata/docker"  # path to Docker build context
+  )
+  result <- system2("docker", docker_cmd, stdout = TRUE, stderr = TRUE)
+  status <- attr(result, "status")
+  if (length(status) > 0) {
+    message(paste("command:", docker_cmd))
+    message(paste("return:", result))
+    stop(paste("Docker command failed with status", status))
+  }
+  return(0)
+}
