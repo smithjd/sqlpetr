a396ac06956c3b30e0ee13029db8383ae8428b31
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-01-24 23:28:27 -0800

    installer for Fedora 29

---
+++ fedora_install.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo dnf install -y \
+  libcurl-devel \
+  libpqxx-devel \
+  libssh2-devel \
+  libxml2-devel \
+  openssl-devel \
+  postgresql-libs \
+  qpdf
+./install_me_linux.R
