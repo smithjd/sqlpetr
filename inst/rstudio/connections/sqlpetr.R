f69f00af90e0e60fbc144c19965c28b89641cea8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-25 19:27:13 -0800

    ditch parameter form

--- inst/rstudio/connections/sqlpetr.R
+++ inst/rstudio/connections/sqlpetr.R
@@ -1,8 +1,4 @@
 library(sqlpetr)
 connection <- sp_get_postgres_connection(
-  user = ${0:user="postgres"},
-  password = ${1:password=""},
-  dbname = ${2:dbname=""},
-  host = ${3:host="localhost"},
-  port = ${4:port=5432},
-  seconds_to_test = ${5:seconds_to_test=30})
+  user, password, dbname, host, port, seconds_to_test
+)
