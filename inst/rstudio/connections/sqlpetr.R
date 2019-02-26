2e637e06c188314583512872312c6ed1c9945acb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-25 19:32:14 -0800

    tweaked comment

--- inst/rstudio/connections/sqlpetr.R
+++ inst/rstudio/connections/sqlpetr.R
@@ -1,5 +1,5 @@
 library(sqlpetr)
-# fill in the parameters, test, then connect
+# fill in the parameters, test, then "OK" to deploy connection
 connection <- sp_get_postgres_connection(
   user, password, dbname, host, port, seconds_to_test
 )
