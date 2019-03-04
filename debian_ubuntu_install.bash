85d9237df9dd8bcb1c59121711beb951bffca3d4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-04 02:44:31 -0800

    devtools dependencies

--- debian_ubuntu_install.bash
+++ debian_ubuntu_install.bash
@@ -7,8 +7,11 @@ curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get install -qqy --no-install-recommends \
   bzip2 \
+  libcurl4-openssl-dev \
   libpq-dev \
   libpqxx-dev \
+  libssh2-1-dev \
+  libssl-dev \
   libxml2-dev \
   postgresql-client-10 \
   pgadmin4 \
