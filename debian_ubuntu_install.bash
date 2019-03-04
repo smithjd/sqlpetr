3e599c6528fc6555d341ba9bdff26f754d8f9410
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-04 01:59:46 -0800

    PostgreSQL 10 and pgAdmin

--- debian_ubuntu_install.bash
+++ debian_ubuntu_install.bash
@@ -5,13 +5,13 @@ echo "Installing prerequisites"
 sudo apt-get install curl ca-certificates
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-xdg-open docs/index.html
 sudo apt-get install -qqy --no-install-recommends \
   bzip2 \
   libpq-dev \
   libpqxx-dev \
   libxml2-dev \
-  postgresql-client \
+  postgresql-client-10 \
+  pgadmin4 \
   qpdf \
   wget \
   zlib1g-dev
