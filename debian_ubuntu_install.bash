2df8efce78f0dee6c46ebff949b496babb6d0db3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-04 03:06:36 -0800

    cleanup

--- debian_ubuntu_install.bash
+++ debian_ubuntu_install.bash
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-sudo apt-get update
 echo "Installing prerequisites"
+sudo apt-get update
 sudo apt-get install curl ca-certificates
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c \
+  'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update
 sudo apt-get install -qqy --no-install-recommends \
   bzip2 \
@@ -19,4 +20,5 @@ sudo apt-get install -qqy --no-install-recommends \
   qpdf \
   wget \
   zlib1g-dev
+echo "Installing R packages"
 ./install_me_linux.R
