501a1aa3e64e75017cdf53c3ead176d284d96f41
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-05 21:53:31 -0800

    fix default after disconnect

--- R/sp_postgresql_functions.R
+++ R/sp_postgresql_functions.R
@@ -267,13 +267,18 @@ sp_pg_catalog <- function(connection) {
 
 .sp_pg_connection_code_string <- function() {
   paste(
-    "library(sqlpetr)",
-    "# fill in the parameters, test, then 'OK' to deploy connection",
-    "connection <- sp_get_postgres_connection(",
-    "user, password, dbname,",
-    "host = 'localhost', port = 5432, seconds_to_test = 30,",
-    "connection_tab = FALSE",
-    ")",
+    'library(sqlpetr)',
+    '# defaults for the "sql-pet" container with "dvdrental" database',
+    '# change the parameter values for your own database configuration',
+    'connection <- sp_get_postgres_connection(',
+    '  user = "postgres",',
+    '  password = "postgres",',
+    '  dbname = "dvdrental",',
+    '  host = "localhost",',
+    '  port = 5432,',
+    '  seconds_to_test = 30,',
+    '  connection_tab = TRUE',
+    ')',
     sep = "\n"
   )
 }
