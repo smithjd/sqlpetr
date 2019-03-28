1ba8ec1af27db81b7efe511d48bf58469067bcf6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-28 02:30:38 -0700

    run postgresql-autodoc

--- inst/extdata/docker/restoredb.sh
+++ inst/extdata/docker/restoredb.sh
@@ -2,3 +2,6 @@ echo "creating the 'dvdrental' database"
 createdb dvdrental
 echo "restoring the database"
 pg_restore -U postgres -d dvdrental /dvdrental.tar
+echo "running postgresql-autodoc"
+cd /var/lib/postgresql
+postgresql_autodoc -d dvdrental -t html
